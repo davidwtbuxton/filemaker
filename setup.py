6e8d8bab6aee253af256ca34818a7df601d18b2c
David Buxton <david@gasmark6.com>
2015-09-06 22:25:09 +0100

    Added a setup script.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name='filemaker',
+    version='0.1',
+    description='Convert Filemaker XML data to Python.',
+    author='David Buxton',
+    author_email='david@gasmark6.com',
+    url='https://github.com/davidwtbuxton/filemaker',
+    py_modules=['filemaker'],
+)
